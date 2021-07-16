2d7de0f75cb7cbf2ee365535319976c1e9d1ace6
邹静磊 <zoujl@shinemo.com>
2021-07-16 14:51:13 +0800

    add triangleDemo

--- filamentJSDemo/src/App.vue
+++ filamentJSDemo/src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+  <TriangleDemo msg = "F" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import TriangleDemo from './components/TriangleDemo.vue' 
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TriangleDemo 
   }
 })
 </script>
